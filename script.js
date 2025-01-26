1cffce1f1bb275fa50be86baeb561e9554db9ef5
0fficial_coderr <bookmac1612@gmail.com>
2025-01-26 10:06:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function submitAnimate(button){
+    let form = document.getElementById("myform")
+
+    button.disabled = true
+
+    form.classList.toggle("load")
+
+    setTimeout(() => {
+        form.classList.replace("load", "submit")
+
+        setTimeout(() =>{
+            form.classList.toggle("submit")
+            form.submit()
+        }, 1500)
+    }, 1500)
+}
